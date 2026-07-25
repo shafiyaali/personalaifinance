33dd80cf8f8c5954f62aa272d372548a37e0057c
shafiyaali <shafiyaali99@gmail.com>
2026-07-25 13:21:12 +0300

    configured Prisma ORM with postgress on docker and designed schema

---
+++ prisma/migrations/20260724165147_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstname" TEXT NOT NULL,
+    "lastname" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "emailVerifiedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
