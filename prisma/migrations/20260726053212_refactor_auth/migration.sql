fc4920d34ad37985b3ca289c6816463a52e713d2
shafiyaali <shafiyaali99@gmail.com>
2026-07-26 08:34:22 +0300

    installed and configured beeter auth with next.js

---
+++ prisma/migrations/20260726053212_refactor_auth/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `emailVerifiedAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `firstName` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "emailVerifiedAt",
+DROP COLUMN "firstName",
+DROP COLUMN "lastName";
