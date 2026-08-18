64a869d4c352648f0f50de5e67c710fcf16b7bc3
shafiyaali <shafiyaali99@gmail.com>
2026-08-18 17:24:02 +0300

    removed unused component

--- features/category/components/CategoryFeatures.tsx
+++ features/category/components/CategoryFeatures.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, {useState} from 'react'
 import CategoryHeader from './categoryHeader'
-import CategoryToolbar from './categoryToolbar'
+// import CategoryToolbar from './categoryToolbar'
 import CategoryTable from './categoryTable'
 import { Category } from '@/generated/prisma/client'
 import CreateCategoryDialog from './createCategoryDialog'
